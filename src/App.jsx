d55fb47f73bb6b6605d1ecaee95bbf8a4d80d446
danielamesaespinal21 <anyimesa2001@gmail.com>
2023-04-09 16:59:20 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ import React from "react";
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
+
+function App() {
+  return (
+      <div className="App">
+          <Navbar/>
+          <Footer/>
+      </div>
+  );
+}
+
+export default App;
